9ac918173446554e8c45d00f81e24a0ca28b1936
Abraham Emmanuel <info@zetamindgroup.com>
2019-12-11 13:19:20 +0100

    Updated: Flush handle method to delete ticket's duration above 100 hours

--- app/Console/Commands/Flush.php
+++ app/Console/Commands/Flush.php
@@ -2,9 +2,10 @@
 
 namespace ComplainDesk\Console\Commands;
 
-use Illuminate\Console\Command;
 use Carbon\Carbon;
 use ComplainDesk\Ticket;
+use ComplainDesk\TicketDuration;
+use Illuminate\Console\Command;
 
 class Flush extends Command
 {
@@ -39,18 +40,45 @@ class Flush extends Command
      */
     public function handle()
     {
-       // Retrieve all tickets from the tickets table
-       $tickets =  Ticket::all();
-       // Get the current date time 
-       $current = Carbon::now();
+        /**
+         * ----TODO's----
+         *   1. Retrieve all tickets from the ticktes table
+         *   2. Loop over the result set
+         *   3. Delete tickets if it has elapsed 3 days
+         */
 
-       // Loop over the tickets resut set 
-       foreach ($tickets as $ticket) {
-           // Check if the tickets has elaspsed 3 days
-           if($current->diffInDays($ticket->created_at) > 9){// if true 
-             // then delete th ticket from the database
-             $ticket->delete();
-           }
-       }
+        // Retrieve all tickets from the tickets table
+        $tickets = Ticket::all();
+        // Get the current date time
+        $current = Carbon::now();
+
+        // Loop over the tickets resut set
+        foreach ($tickets as $ticket) {
+            // Check if the tickets has elaspsed 3 days
+            if ($current->diffInDays($ticket->created_at) > 9) { // if true
+                // then delete the ticket from the database
+                $ticket->delete();
+            }
+        }
+
+        /**
+         * ----TODO's----
+         *   1. Retrieve all tickets_duration from the tickets_duration table
+         *   2. Loop over the result set
+         *   3. Delete tickets_duration if the duration is greater than 100
+         */
+
+        // Retrieve all tickets_duration from ticket_duration table
+        $tickets_duration = TicketDuration::all();
+
+        //Loop over the result set
+        foreach ($tickets_duration as $ticket_duration) {
+            // Check if the tickets has elaspsed 3 days
+            if ($ticket_duration > 100) { // if true
+                // then delete the ticket_duration from the database
+                $ticket_duration->delete();
+            }
+        }
+
     }
 }
